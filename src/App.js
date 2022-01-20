7a1d4f83276680f04e270e42e4e7b8495ba30434
karam baaj <baaj.kr@gmail.com>
2022-01-20 17:54:06 +0400

    creating post and posts files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React  from 'react';
+import Posts from './containers/posts';
 
 
 
 
 function app() {
-  return ( <div>my blog</div> );
+  return (
+    <div>
+      <Posts />
+    </div> );
 }
 
 export default app;
